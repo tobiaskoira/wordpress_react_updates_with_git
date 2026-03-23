801ed42bd7eef57e02cd1ba2ef24fdbda8be9021
Dinara <ab3825@student.jamk.fi>
2026-03-23 10:40:23 +0200

    sidebar position changes

--- page-profile.php
+++ page-profile.php
@@ -2,7 +2,7 @@
 
 
 
-<div id="react-profile" class="w-full max-w-screen-xl mx-auto p-4 md:py-8"></div>
+<div id="react-profile" class="w-full max-w-screen-xl mx-auto pt-24 md:py-8"></div>
            
 
 
