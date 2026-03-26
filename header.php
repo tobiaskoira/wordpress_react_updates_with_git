b441764d932a74ab8d18012f5f02e0afb73e6a08
Dinara <ab3825@student.jamk.fi>
2026-03-26 13:16:08 +0200

    auth menu development

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 				</button>
                 <!-- <div id="react-auth-button"></div> -->
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="hidden md:flex text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium rounded-base text-base px-5 py-3 focus:outline-none" type="button">
-                Sign in account 
+               My account 
                  <svg class="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/></svg>
                 </button>
 
